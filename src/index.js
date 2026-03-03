088dc605d28efb9cd8090283b49ff003966228be
Programmer6519 <faizansaith401@gmail.com>
2026-03-03 15:20:22 +0500

    util Code Disscussion

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from 'dotenv'
-dotenv.config({path: './env'});
+import cors from 'cors'
 // require('dotenv').config({path: './env'});
 
 
 const app = express();
-connectDB();
+dotenv.config({path: './env'});
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening at port ${process.env.PORT}`);
-});
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true,limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is listeneing to port ${process.env.PORT}`);
+  })
+})
+.catch((err) => {console.log(err)});
+
 
 /*
 import express from 'express';
