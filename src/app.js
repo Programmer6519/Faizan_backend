6e6eb97b0143f6cc2aa2bf8171445e0d6633f7db
Programmer6519 <faizansaith401@gmail.com>
2026-03-04 15:01:08 +0500

    cloudinary and multer configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
-import express from 'express';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import express, { urlencoded } from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
+// require('dotenv').config({path: './env'});
+
 
 const app = express();
+dotenv.config({path: './env'});
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
 
-export { app } 
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true,limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-import cookieParser from "cookie-parser";
-import connectDB from "./config/db.js";
-import express, { urlencoded } from "express";
-import dotenv from 'dotenv'
-import cors from 'cors'
-// require('dotenv').config({path: './env'});
-
-
-const app = express();
-dotenv.config({path: './env'});
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
-
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true,limit: "16kb"}));
-app.use(express.static("public"));
-app.use(cookieParser());
-
-
+import connectDB from './config/db.js';
+import app from './app.js';
 
 
 connectDB()
 .then(() => {
-  app.listen(process.env.PORT || 8000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is listeneing to port ${process.env.PORT}`);
   })
 })
